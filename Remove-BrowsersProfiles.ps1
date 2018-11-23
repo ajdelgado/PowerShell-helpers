268723d5133777bb0e42040b88ba30f6b5d3b3a1
Antonio J. Delgado <adelgado@greenpeace.org>
2018-11-23 10:40:11 +0100

    Create Remove-BrowsersProfiles.ps1

---
+++ Remove-BrowsersProfiles.ps1
@@ -0,0 +1,16 @@
+New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+foreach ($user in (Get-LocalUser)) {
+    if ($user.Enabled) {
+        $userid=$user.SID
+        $userprofile=(get-itemproperty "HKU:\$userid\volatile environment" -Name USERPROFILE).USERPROFILE
+        if ($userprofile) {
+            write-host "Profile for '$user' is '$userprofile'"
+            # Remove all Google Chrome configurations
+            remove-item "$userprofile\AppData\Local\Google\Chrome\User Data" -Recurse -Force
+            # Remove all Mozilla applications' configurations
+            remove-item "$userprofile\AppData\Local\Mozilla" -Recurse -Force
+            # Remove all temp files
+            remove-item "$userprofile\AppData\Local\Temp\*" -Recurse -Force
+        }
+    }
+}
