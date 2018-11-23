2ae1c3ed7598a4ecbf320ca32cff94757077f34d
Antonio J. Delgado <adelgado@greenpeace.org>
2018-11-23 10:52:31 +0100

    Fixed HKU path

--- Remove-BrowsersProfiles.ps1
+++ Remove-BrowsersProfiles.ps1
@@ -1,10 +1,10 @@
 New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
 foreach ($user in (Get-LocalUser)) {
     if ($user.Enabled) {
-        $userid=$user.SID
-        $userprofile=(get-itemproperty "HKU:\$userid\volatile environment" -Name USERPROFILE).USERPROFILE
+        $userid=$user.SID.AccountDomainSid
+        $userprofile=(get-itemproperty "HKU:\$userid-500\volatile environment" -Name USERPROFILE).USERPROFILE
         if ($userprofile) {
-            write-host "Profile for '$user' is '$userprofile'"
+            write-host "Profile for '$user' ($userid) is '$userprofile'"
             # Remove all Google Chrome configurations
             remove-item "$userprofile\AppData\Local\Google\Chrome\User Data" -Recurse -Force
             # Remove all Mozilla applications' configurations
